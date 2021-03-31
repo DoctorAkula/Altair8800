5b82fcd12207d70c33a59997a0a2dc971d9a8ced
Bob <atomicsteamend@gmail.com>
2021-03-30 19:28:11 -0700

    wcomp.sh console based hostname/user input for wcomp.sh

--- src/wcomp.sh
+++ src/wcomp.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 CC="x86_64-w64-mingw32-gcc"
 libs="-lmingw32 -lglfw3dll -lraylib -lm"
-$CC  -o /tmp/Altair8800.exe 8080.c panel.c mem.c main.c $libs && scp /tmp/Altair8800.exe gamer@stuka:
+$CC  -o /tmp/Altair8800.exe 8080.c panel.c mem.c main.c $libs && scp /tmp/Altair8800.exe $1@$2:
